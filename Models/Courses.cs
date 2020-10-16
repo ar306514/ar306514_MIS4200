6c0f9ff93044aee415731a7181bb929f0d340d80
Risner, Alexis <ar306514@ohio.edu>
2020-10-15 21:26:10 -0400

    SD2 halfway

---
+++ Models/Courses.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace ar306514_MIS4200.Models
+{
+    public class Courses
+    {
+        
+        [Key]
+        public int courseId { get; set; }
+        public int title { get; set; }
+        public string description { get; set; }
+
+        // add any other fields as appropriate
+        //Product is on the "one" side of a one-to-many relationship with OrderDetail
+        //we indicate that with an ICollection
+        public ICollection<Enrollments> enrollments { get; set; }
+
+    }
+}
---
+++ Models/Enrollments.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ar306514_MIS4200.Models
+{
+    public class Enrollments
+    {
+        [Key]
+        public int enrollmentId { get; set; }
+        public string studentId { get; set; }
+        public DateTime classId { get; set; }
+        // add any other fields as appropriate
+        //Order is on the "one" side of a one-to-many relationship with OrderDetail
+        //and we indicate that with an ICollection
+        //public ICollection<OrderDetail> OrderDetail { get; set; }
+        //Order is on the Many side of the one-to-many relation between Customer
+        //and Order and we represent that relationship like this
+        public int studentID { get; set; }
+        public virtual Students Students { get; set; }
+
+        public int courseId { get; set; }
+        public virtual Courses Courses { get; set; }
+
+    }
+}
